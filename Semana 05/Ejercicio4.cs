522952e34af80c49e5e66a7d941ca76f9775e1de
Liss Troya <yl.troyay@uea.edu.ec>
2026-01-07 12:57:14 -0500

    Ejercicos de listas y tuplas en C#

---
+++ Semana 05/Ejercicio4.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class Ejercicio4
+{
+    public void Ejecutar()
+    {
+        List<int> numeros = new List<int>();
+    
+    //Pedir 10 numeros de la loteria y mostralos ordenados
+        Console.WriteLine("Ingrese 5 números de la lotería:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros.Add(int.Parse(Console.ReadLine()));
+        }
+    //Ordenar la lista de menor a mayor
+        numeros.Sort();
+
+        Console.WriteLine("Números ordenados de menor a mayor:");
+        Console.WriteLine(string.Join(", ", numeros));
+    }
+}
